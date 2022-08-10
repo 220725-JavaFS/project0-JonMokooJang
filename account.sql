abb214d33aa21136bae46c5b7953faad6c087058
JonMokooJang <jonjang299@gmail.com>
2022-08-10 01:14:35 -0400

    Added sql file

---
+++ account.sql
@@ -0,0 +1,23 @@
+drop table if exists account;
+
+--This indicates account information
+
+create table account (
+	account_Number SERIAL primary key, 
+	account_User varchar(30) not null,
+	account_Pass varchar(20) not null,
+	account_First_Name varchar(20),
+	account_Last_Name varchar(30),
+	account_Job varchar(20),
+	account_balance int not null
+);
+	
+insert into account (account_user , account_pass , account_first_name, account_last_name, account_job, account_balance) values
+	('test', 'test', 'Joseph', 'Giovanna', 'Researcher', 5000),
+	('testtwo', 'testtwo', 'Jonathan', 'Joestar', 'Historian', 10000);
+	
+
+SELECT * FROM account where account_user='test'
+
+
+
